c850fd5b2fbd06e3193b413f08f58b1989c621ee
shalomidoga <shalomidoga@gmail.com>
2026-05-03 22:52:22 +0200

    upda

--- config/mail.php
+++ config/mail.php
@@ -45,6 +45,7 @@ return [
             'port' => env('MAIL_PORT', 2525),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
+            'auth_mode' => env('MAIL_AUTH_MODE', 'login'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
